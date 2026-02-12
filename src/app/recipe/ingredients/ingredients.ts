6394c8d79450825893aa8038986a583c8a4e6f8c
Dominik <gilges.dominik@gmail.com>
2026-02-12 15:10:10 +0100

    more base-line design ideas, moved components around, properly implemented data from backend

--- src/app/recipe/ingredients/ingredients.ts
+++ src/app/recipe/ingredients/ingredients.ts
@@ -1,10 +1,13 @@
-import { Component } from '@angular/core';
+import { Component, input } from '@angular/core';
 import { MatCardModule } from '@angular/material/card';
+import { MatListModule } from '@angular/material/list';
 
 @Component({
   selector: 'app-ingredients',
-  imports: [MatCardModule],
+  imports: [MatCardModule, MatListModule],
   templateUrl: './ingredients.html',
   styleUrl: './ingredients.scss',
 })
-export class Ingredients {}
+export class Ingredients {
+  public ingredients = input<string[] | undefined>(['ingredients loading...']);
+}
--- src/app/recipe/instructions/instructions.ts
+++ src/app/recipe/instructions/instructions.ts
@@ -1,10 +1,13 @@
-import { Component } from '@angular/core';
+import { Component, input } from '@angular/core';
 import { MatCardModule } from '@angular/material/card';
+import { MatListModule } from '@angular/material/list';
 
 @Component({
   selector: 'app-instructions',
-  imports: [MatCardModule],
+  imports: [MatCardModule, MatListModule],
   templateUrl: './instructions.html',
   styleUrl: './instructions.scss',
 })
-export class Instructions {}
+export class Instructions {
+  public instructions = input<string[] | undefined>(['instructions loading...']);
+}
