f50d4e1146a55fb5e250c40bf6357a49c5982afb
Dominik <gilges.dominik@gmail.com>
2026-02-17 13:05:09 +0100

    minor css: fixed selection component to keep a fixed sizes so it looks less wonky. fixed minor bug where selection component was not centered when recipe component is not displayed.

--- src/app/app.ts
+++ src/app/app.ts
@@ -1,9 +1,10 @@
-import { Component, signal } from '@angular/core';
+import { Component, inject, signal } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
 import { Header } from './header/header';
 import { Selection } from './selection/selection';
 import { Recipe } from './recipe/recipe';
 import { Footer } from './footer/footer';
+import { RecipesService } from './recipes.service';
 
 @Component({
   selector: 'app-root',
@@ -14,4 +15,5 @@ import { Footer } from './footer/footer';
 export class App {
   protected readonly title = signal('foodieflip');
   protected readonly version = signal('0.1');
+  public readonly recipesService = inject(RecipesService);
 }
