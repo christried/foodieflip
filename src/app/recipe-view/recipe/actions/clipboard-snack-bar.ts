0e038dfe074b27e158222d5a639fdf4c637f30bd
Chris <40856872+christried@users.noreply.github.com>
2026-03-09 18:29:55 +0100

    - added environment files for prod for a later point in time, need to add correct prod url there - added button to copy recipe url to clipboard to share it somewhere else - added snackbar to copy to clipboard button

---
+++ src/app/recipe-view/recipe/actions/clipboard-snack-bar.ts
@@ -0,0 +1,32 @@
+import { Component, inject } from '@angular/core';
+import { MatButtonModule } from '@angular/material/button';
+import { MatIconModule } from '@angular/material/icon';
+import {
+  MatSnackBarAction,
+  MatSnackBarActions,
+  MatSnackBarLabel,
+  MatSnackBarRef,
+} from '@angular/material/snack-bar';
+
+@Component({
+  selector: 'clipboard-snack-bar',
+  templateUrl: 'clipboard-snack-bar.html',
+  styles: `
+    :host {
+      display: flex;
+    }
+    .remove-icon {
+      color: black;
+    }
+  `,
+  imports: [
+    MatButtonModule,
+    MatSnackBarLabel,
+    MatSnackBarActions,
+    MatSnackBarAction,
+    MatIconModule,
+  ],
+})
+export class ClipboardSnackBar {
+  snackBarRef = inject(MatSnackBarRef);
+}
---
+++ src/environments/enfironment.prod.ts
@@ -0,0 +1,4 @@
+// to be replaced with prod URL hopefully soon
+export const environment = {
+  baseUrl: 'http://localhost:4200',
+};
