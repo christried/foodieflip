5c16d8f0cfc26605550a3a5192d07adaea57b007
Chris <40856872+christried@users.noreply.github.com>
2026-03-10 17:34:16 +0100

    added professional content for 404 subpage

--- src/app/pages/not-found/not-found.ts
+++ src/app/pages/not-found/not-found.ts
@@ -1,11 +1,10 @@
 import { Component } from '@angular/core';
+import { MatCardModule } from '@angular/material/card';
 
 @Component({
   selector: 'app-not-found',
-  imports: [],
+  imports: [MatCardModule],
   templateUrl: './not-found.html',
-  styleUrl: './not-found.scss',
+  styleUrls: ['./not-found.scss', '../pages.scss'],
 })
-export class NotFound {
-
-}
+export class NotFound {}
