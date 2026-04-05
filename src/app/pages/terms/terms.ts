fe37ba5eb62a677f1c6e04d5e497c44764bdffd5
Chris <40856872+christried@users.noreply.github.com>
2026-04-05 17:56:54 +0200

    minor additions and fixes for /terms page

--- src/app/pages/terms/terms.ts
+++ src/app/pages/terms/terms.ts
@@ -1,9 +1,10 @@
 import { Component } from '@angular/core';
 import { MatCardModule } from '@angular/material/card';
+import { RouterLink } from '@angular/router';
 
 @Component({
   selector: 'app-terms',
-  imports: [MatCardModule],
+  imports: [MatCardModule, RouterLink],
   templateUrl: './terms.html',
   styleUrls: ['./terms.scss', '../pages.scss'],
 })
