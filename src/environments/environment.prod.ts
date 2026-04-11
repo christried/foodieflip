9bb3d502f40b326148dbc41d6e4d4df7cfcff53b
Chris <40856872+christried@users.noreply.github.com>
2026-04-11 14:25:49 +0200

    switched backend url to https://api.foodieflip.app for same-site cookie policy

--- src/environments/environment.prod.ts
+++ src/environments/environment.prod.ts
@@ -1,5 +1,5 @@
 export const environment = {
   baseUrl: 'https://foodieflip.app',
-  apiBaseUrl: 'https://foodieflip-api-f2efdc3a06e8.herokuapp.com',
+  apiBaseUrl: 'https://api.foodieflip.app',
   googleClientId: '675010679890-qrdvqv7t8urdug06e3824iq438e7ari3.apps.googleusercontent.com',
 };
